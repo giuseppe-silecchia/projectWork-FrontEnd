84c1555e15f769079152d87db631f4832f3e3bcb
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-03 12:46:16 +0100

    Implemented register component UI

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+
+  constructor(private router: Router) {
+  }
+
+
+  goToLogin(): void {
+    this.router.navigate(['login']);
+  }
 }
