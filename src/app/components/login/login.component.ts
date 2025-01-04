aacb6ae584ec39faed8c4bfb4233423bee8cdc1a
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-04 18:01:46 +0100

    Implemented authService (login) in Login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,10 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {NgIf} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, NgForm} from '@angular/forms';
+import {LogIn} from '../../models/logIn';
+import {AuthService} from '../../services/auth.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -13,11 +16,34 @@ import {FormsModule} from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router) {
+  logIn: LogIn = <LogIn>{};
+
+  constructor(private router: Router, private authService: AuthService, private toasterService: ToastrService) {
   }
 
 
+  submitLogin(loginForm: NgForm): void {
+    if (loginForm.invalid) return;
+
+    this.authService.login(this.logIn).subscribe({
+      next: () => {
+        loginForm.resetForm();
+        this.goToHome()
+        this.toasterService.success("", "Accesso effettuato!", {timeOut: 2000});
+      },
+      error: (error: any) => {
+        if (error.status === 401) {
+          this.toasterService.error("Credenziali Errate!", "Errore");
+        } else this.toasterService.error("Errore! Riprovare.", "Errore");
+      }
+    });
+  }
+
   goToRegister(): void {
     this.router.navigate(['register']);
+  }
+
+  goToHome(): void {
+    this.router.navigate(['']);
   }
 }
