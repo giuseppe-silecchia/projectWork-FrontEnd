47f60f4ba473ecfe9eef51d9560b10dee0ddd523
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-04 18:00:40 +0100

    Implemented login method in authService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,22 +1,45 @@
 import {Injectable} from '@angular/core';
 import {SignIn} from '../models/signIn';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {map, Observable} from 'rxjs';
 import {environment} from '../environment';
+import {LogIn} from '../models/logIn';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = environment.apiUrl+"/api";
+  private baseUrl = environment.apiUrl + "/api";
+  private storageKey = "authToken";
+
 
   constructor(private http: HttpClient) {
   }
 
-
-  register(signIn: SignIn): Observable<Object> {
-    return this.http.post(`${this.baseUrl}/register`, signIn)
+  login(login: LogIn): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, login).pipe(
+      map((response: any) => {
+        if (response.access_token) {
+          this.setAuthToken(response.access_token);
+        }
+        return response;
+      }),
+    );
   }
 
+  register(signIn: SignIn): Observable<Object> {
+    return this.http.post(`${this.baseUrl}/register`, signIn);
+  }
 
+  getAuthToken(): string | null {
+    return localStorage.getItem(this.storageKey);
+  }
+
+  private setAuthToken(token: string) {
+    localStorage.setItem(this.storageKey, token);
+  }
+
+  private signOut() {
+    localStorage.removeItem(this.storageKey);
+  }
 }
