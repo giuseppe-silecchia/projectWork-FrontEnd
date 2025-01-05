6d04b66c91b66f2736798eadede042cafe742236
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-05 18:53:09 +0100

    Added room property to Booking model

--- src/app/models/booking.ts
+++ src/app/models/booking.ts
@@ -1,6 +1,10 @@
+import {Room} from './room';
+
 export interface Booking {
+  id?: number;
   check_in: string;
   check_out: string;
   room_id: number;
-  people:number;
+  people: number;
+  room?: Room | null;
 }
