a46de1dbe1da069a4c9339e3973428d216f652d0
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-05 11:10:45 +0100

    Added authInterceport in App configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,23 @@
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideToastr} from 'ngx-toastr';
+import {AuthInterceptor} from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
     provideAnimations(),
-    provideToastr({positionClass: 'toast-top-center', progressBar:true, progressAnimation:'decreasing',timeOut:5000}),
+    provideToastr({
+      positionClass: 'toast-top-center',
+      progressBar: true,
+      progressAnimation: 'decreasing',
+      timeOut: 5000
+    }),
   ]
 };
