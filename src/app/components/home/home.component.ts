43969d4993297cef646d52813319307fc405f63b
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-05 12:04:42 +0100

    Updated navbar component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from '../navbar/navbar.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    RouterOutlet
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';
+import {NgbCollapse} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    NgbCollapse
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isCollapsed = false;
+
+  constructor(private authService: AuthService, private toasterService: ToastrService, private router: Router,) {
+  }
+
+  goToHome(): void {
+    this.router.navigate(['/']);
+  }
+
+  logout(): void {
+    this.authService.signOut();
+    this.router.navigate(['login']);
+    this.toasterService.info('Logout effettuato!');
+  }
+
+  goToBookPage(): void {
+    this.router.navigate(['prenota']);
+  }
+
+  currentRoute(route: String): boolean {
+    return this.router.url === route;
+  }
 
 }
