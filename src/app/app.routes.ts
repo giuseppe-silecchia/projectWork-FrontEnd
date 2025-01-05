79797f4dac6da3a460d40861242e4b9f85152ff6
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-05 12:04:23 +0100

    Added BookingComponent in routes (home subroute)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,17 @@ import {NotFoundComponent} from './components/not-found/not-found.component';
 import {RegisterComponent} from './components/register/register.component';
 import {HomeComponent} from './components/home/home.component';
 import {AuthGuard} from './guards/auth.guard';
+import {BookingComponent} from './components/booking/booking.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  {
+    path: '', component: HomeComponent, canActivate: [AuthGuard], children: [
+      {
+        path: 'prenota', // Sotto rotta
+        component: BookingComponent,
+      },
+    ],
+  },
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'notfound', component: NotFoundComponent},
