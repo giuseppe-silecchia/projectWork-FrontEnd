d2cb626a47d6bb8eea1a1a4d7f5ae0c66c66da5b
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-05 18:01:57 +0100

    Removed useless log in auth interceptor

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,19 +1,18 @@
 import {catchError, Observable, throwError} from 'rxjs';
 import {AuthService} from '../services/auth.service';
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService,private router: Router) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.authService.getAuthToken();
 
     if (token) {
-      console.log("intercept working");
       request = request.clone({
         setHeaders: {Authorization: `Bearer ${token}`}
       });
