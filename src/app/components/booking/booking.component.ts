5aaf6cf66b0ce5c5bb16f50f09ff021aca8c58c0
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-05 12:03:57 +0100

    Created BookingComponent

---
+++ src/app/components/booking/booking.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  imports: [],
+  templateUrl: './booking.component.html',
+  styleUrl: './booking.component.css'
+})
+export class BookingComponent {
+
+}
