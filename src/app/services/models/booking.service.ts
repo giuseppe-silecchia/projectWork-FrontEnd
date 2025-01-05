7a44936c89c40a5caa841b6751411850ceb88c80
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-05 18:57:17 +0100

    Implemented getUserBookings in BookingService

--- src/app/services/models/booking.service.ts
+++ src/app/services/models/booking.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
 import {environment} from '../../environment';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {catchError, forkJoin, map, Observable, switchMap} from 'rxjs';
 import {Booking} from '../../models/booking';
+import {RoomService} from './room.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,36 @@ import {Booking} from '../../models/booking';
 export class BookingService {
   private bookingsEndpoint = environment.apiUrl + '/bookings';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private roomService: RoomService) {
   }
 
   createBooking(booking: Booking): Observable<Object> {
     return this.http.post(this.bookingsEndpoint, booking);
+  }
+
+  getUserBookings(): Observable<Booking[]> {
+    /*
+    * Recupero le prentoazioni dell'utente e uso lo switchmap per passare il flusso delle prenotazioni al flusso per
+    * recuperare anche i dettagli della stanza associata
+    * per ogni prenotazione vado a recuperare i dettagli della stanza e in caso di errore la properietà 'room' sarà null.
+    * Infine con la forkJoin attendo che tutti gli observables siano completati e restituisco un array di Prenotazioni
+    * */
+
+    return this.http.get<Booking[]>(this.bookingsEndpoint + '/user').pipe(switchMap(bookings => {
+      const bookingsWithRooms = bookings.map(booking =>
+        this.roomService.getRoom(booking.room_id).pipe(
+          map(room => ({...booking, room})),
+          catchError(() => {
+            // Se c'è un errore, restituisci il booking con room = null
+            return [{...booking, room: null}];
+          })
+        )
+      );
+      return forkJoin(bookingsWithRooms);
+    }));
+  }
+
+  cancelBooking(booking: Booking): Observable<Object> {
+    return this.http.delete(`${this.bookingsEndpoint}/${booking.id}`);
   }
 }
