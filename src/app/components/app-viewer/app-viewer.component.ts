dd0fc9f0b1aba501cce3f38c00c520c3fba7734e
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-05 19:43:20 +0100

    Added loader in app-viewer component

--- src/app/components/app-viewer/app-viewer.component.ts
+++ src/app/components/app-viewer/app-viewer.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {NavbarComponent} from '../navbar/navbar.component';
 import { RouterOutlet} from '@angular/router';
+import {NgxSpinnerComponent} from 'ngx-spinner';
 
 @Component({
   selector: 'app-viewer',
   imports: [
     NavbarComponent,
     RouterOutlet,
+    NgxSpinnerComponent,
   ],
   templateUrl: './app-viewer.component.html',
   styleUrl: './app-viewer.component.css'
