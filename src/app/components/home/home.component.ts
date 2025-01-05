fec0c04772b63b861cf2e864b178827000e02f01
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-05 13:07:20 +0100

    Added homepage UI

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from '../navbar/navbar.component';
-import {RouterOutlet} from '@angular/router';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    NavbarComponent,
-    RouterOutlet
-  ],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  constructor(private router: Router) {
+  }
+
+  goToBookingPage(){
+    this.router.navigate(['prenota']);
+  }
+
+  goToMyReservations(){}
 }
