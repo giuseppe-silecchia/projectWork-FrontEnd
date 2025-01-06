8f98bf1b5eb1db1a5f4fef52e30883ab9097cca0
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-06 16:03:53 +0100

    Added admin methods in bookingService

--- src/app/services/models/booking.service.ts
+++ src/app/services/models/booking.service.ts
@@ -42,6 +42,30 @@ export class BookingService {
     }));
   }
 
+  getAllBookings(): Observable<Booking[]> {
+    /*
+    * Recupero le prentoazioni del sistema e uso lo switchmap per passare il flusso delle prenotazioni al flusso per
+    * recuperare anche i dettagli della stanza associata
+    * per ogni prenotazione vado a recuperare i dettagli della stanza e in caso di errore la properietà 'room' sarà null.
+    * Infine con la forkJoin attendo che tutti gli observables siano completati e restituisco un array di Prenotazioni
+    * */
+
+    return this.http.get<Booking[]>(this.bookingsEndpoint).pipe(switchMap(bookings => {
+      if (bookings.length === 0) return of([]);
+
+      const bookingsWithRooms = bookings.map(booking =>
+        this.roomService.getRoom(booking.room_id).pipe(
+          map(room => ({...booking, room})),
+          catchError(() => {
+            // Se c'è un errore, restituisci il booking con room = null
+            return [{...booking, room: null}];
+          })
+        )
+      );
+      return forkJoin(bookingsWithRooms);
+    }));
+  }
+
   cancelBooking(booking: Booking): Observable<Object> {
     return this.http.delete(`${this.bookingsEndpoint}/${booking.id}`);
   }
