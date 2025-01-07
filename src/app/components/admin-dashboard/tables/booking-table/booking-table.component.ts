3251f3cb33628c1dd399bb9b7b3a2f65b1400828
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-07 19:03:08 +0100

    booking-table component added remove booking button

--- src/app/components/admin-dashboard/tables/booking-table/booking-table.component.ts
+++ src/app/components/admin-dashboard/tables/booking-table/booking-table.component.ts
@@ -1,7 +1,10 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Booking} from '../../../../models/booking';
 import {User} from '../../../../models/user';
 import {DatePipe, NgForOf} from '@angular/common';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {ToastrService} from 'ngx-toastr';
+import {BookingService} from '../../../../services/models/booking.service';
 
 @Component({
   selector: 'app-booking-table',
@@ -15,9 +18,34 @@ import {DatePipe, NgForOf} from '@angular/common';
 export class BookingTableComponent {
   @Input() bookings: Booking[] = [];
   @Input() users: User[] = [];
+  @Output("reloadParentData") reloadParentData: EventEmitter<any> = new EventEmitter();
+
+  constructor(private bookingService: BookingService, private loaderService: NgxSpinnerService, private toastrService: ToastrService) {
+  }
 
   getPersonNameFromBooking(booking: Booking): String {
     const user = this.users.find(u => u.id === booking.user_id);
     return user ? `${user.first_name} ${user.last_name}` : 'Utente non trovato';
+  }
+
+  deleteBooking(booking: Booking) {
+    this.loaderService.show();
+
+    this.bookingService.cancelBooking(booking).subscribe(
+      {
+        next: () => {
+          this.loaderService.hide();
+          this.toastrService.success('Stanza eliminata con successo!');
+          this.reloadParentData.emit();
+        },
+        error: (error) => {
+          this.loaderService.hide();
+          if (error.status === 500) {
+            this.toastrService.error('Errore.', `La stanza ha delle prenotazioni associate!`)
+          } else
+            this.toastrService.error('Errore.', 'Riprovare.')
+        }
+      }
+    );
   }
 }
