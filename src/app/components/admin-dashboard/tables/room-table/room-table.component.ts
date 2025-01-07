1ef4c88080cc41753a2af93901e6cb92137e8927
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-07 23:15:23 +0100

    Added detailed comments in room table component

--- src/app/components/admin-dashboard/tables/room-table/room-table.component.ts
+++ src/app/components/admin-dashboard/tables/room-table/room-table.component.ts
@@ -18,16 +18,34 @@ import {RoomService} from '../../../../services/models/room.service';
   styleUrl: './room-table.component.css'
 })
 export class RoomTableComponent {
-  @Input() rooms: Room[] = [];
+  @Input() rooms: Room[] = []; // Array di stanze che viene passato al componente dal componente padre.
+
+  /* Emette un evento al componente genitore per aggiornare i dati.
+  * Utilizzato per notificare al componente genitore di ricaricare i dati
+  */
   @Output("reloadParentData") reloadParentData: EventEmitter<any> = new EventEmitter();
 
   roomToCreate: Room = <Room>{max_people: 1};
   selectedRoomToEdit: Room | null = null;
   roomToEdit?: Room | null = null;
 
+  /*
+  * Il componente RoomTableComponent riceve un array di stanze tramite proprietà @Input().
+  * Visualizza una tabella con tutti le stanze presenti nel sistema.
+  * La tabella permette all'utente amministratore di eliminare, aggiungere e modificare una qualsiasi stanza
+* */
+
+
   constructor(private roomService: RoomService, private loaderService: NgxSpinnerService, private toastrService: ToastrService) {
   }
 
+  /*
+  * Questo metodo gestisce la creazione di una nuova stanza.
+  * Se il modulo è valido, invia i dati al server (tramite il roomService) per aggiungere la stanza.
+  * Se la stanza è stata creata correttamente, viene notificato un successo
+  * il modulo viene resettato e vengono aggiornati i dati.
+  * In caso di errore, viene visualizzato un messaggio di errore appropriato.
+  * */
   submitCreateRoom(createRoomForm: NgForm): void {
     if (createRoomForm.invalid) return;
 
@@ -48,6 +66,7 @@ export class RoomTableComponent {
     });
   }
 
+  // Questo metodo gestisce l'aggiornamento di una stanza esistente.
   submitEditRoom(editRoomForm: NgForm): void {
     if (editRoomForm.invalid && !this.roomToEdit) return;
 
@@ -70,6 +89,7 @@ export class RoomTableComponent {
     );
   }
 
+  // Il metodo viene invocato quando una stanza viene selezionata (dal form per modificare una stanza).
   onRoomSelected(room: Room) {
     if (room) {
       this.selectedRoomToEdit = room;
@@ -77,6 +97,7 @@ export class RoomTableComponent {
     }
   }
 
+  // Gestisce l'eliminazione di una stanza.
   deleteRoom(room: Room) {
     this.loaderService.show();
 
